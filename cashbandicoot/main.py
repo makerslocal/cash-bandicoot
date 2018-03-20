1cafbc37501d85fcf9febee6fd2d5b983ca7a6ff
Tyler Crumpton <tyler.crumpton@gmail.com>
2018-03-20 00:53:28 -0500

    Start feeling out some models and routes.

--- cashbandicoot/main.py
+++ cashbandicoot/main.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
+users = {
+    1: {
+        "uid": "1322541",
+        "realname": "Tyler Crumpton",
+        "username": "tylercrumpton",
+        "balance": 1350,
+    },
+}
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+
+@app.route("/users")
+def get_users():
+    return jsonify(users)
+
+
+@app.route("/users", methods=['POST'])
+def add_user():
+    user = {
+        "uid": "8885412",
+        "realname": "Tayler Crompton",
+        "username": request.form['username'],
+        "balance": 0,
+    }
+    users[2] = user
+
+    return jsonify(user)
