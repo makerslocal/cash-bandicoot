3b714390ed2ea6b6ebb44b066d83e7f0a4bc4bc6
Hunter Fuller <hfuller@pixilic.com>
2018-04-04 00:54:45 -0500

    Remove test stuff and add real get-user and add-transaction routes
    We just implicitly add the user when they get their first transaction.
    
    Also, I'm thinking we can probably get the user via any arbitrary
    attribute,  which can be passed in on get parameters on the "/user"
    route. Right now, there is a shortcut kinda-sugary route also,
    which is "/user/<username>" - much better for usage by humans.
    
    I'll have to talk to Tyler about how that's going to work.

--- cashbandicoot/main.py
+++ cashbandicoot/main.py
@@ -9,15 +9,6 @@ with open('config.json', 'r') as f:
 
 db = psycopg2.connect(host=config['database']['host'], database=config['database']['name'], user=config['database']['role'], password=config['database']['password'], cursor_factory=psycopg2.extras.RealDictCursor)
 
-users = {
-    1: {
-        "uid": "1322541",
-        "realname": "Tyler Crumpton",
-        "username": "tylercrumpton",
-        "balance": 1350,
-    },
-}
-
 
 @app.route("/users")
 def get_users():
@@ -28,15 +19,49 @@ def get_users():
         ''')
         return jsonify(cur.fetchall())
 
+@app.route("/user")
+@app.route("/user/<username>")
+def get_user(username=None):
+    if username is None:
+        raise NotImplementedError("This is where we would use request.args to search LDAP, matching the arbitrary filter you provide in order to find the username, then we would return get_user(username=that_user).")
+    else:
+        with db.cursor() as cur:
+            cur.execute('''
+                select username, balance
+                from person
+                where username = %s
+            ''', [username])
+            return jsonify(cur.fetchone())
 
-@app.route("/users", methods=['POST'])
-def add_user():
-    user = {
-        "uid": "8885412",
-        "realname": "Tayler Crompton",
-        "username": request.form['username'],
-        "balance": 0,
-    }
-    users[2] = user
+@app.route("/transaction", methods=["POST"])
+def add_transaction():
+    username = request.form['username']
+    amount = float(request.form['amount'])
 
-    return jsonify(user)
+    with db.cursor() as cur:
+        #In psycopg2, "with" clauses like this will automatically BEGIN TRANSACTION.
+
+        #First, try to make a new user, with the amount as their starting balance.
+        #If there is a conflict, we take the number we just TRIED to use as their starting balance,
+        #and instead, we add it to their existing balance, and update their row with that.
+        cur.execute('''
+            insert into person(username, balance)
+            values(%s, %s)
+            on conflict(username) do
+            update set balance = person.balance + EXCLUDED.balance
+        ''', [username, amount])
+
+        #Now we insert the transaction row.
+        cur.execute('''
+            insert into transaction(service_id, person_id, amount)
+            values(
+                1,
+                (select id from person where username = 'hfuller'),
+                0.50
+            )
+        ''', [username])
+
+        #Now we're done.
+        db.commit()
+    
+    return get_user(username=username)
