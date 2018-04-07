faada59f1dc8834942e4cfd383ec301bf6e87267
Hunter Fuller <hfuller@pixilic.com>
2018-04-07 00:36:48 -0500

    Add mechanism to require a service auth token to do stuff.
    Right now, every single action requires the token. We'll have to
    provide other methods of user auth later. Or something

--- cashbandicoot/main.py
+++ cashbandicoot/main.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 import json
 import psycopg2, psycopg2.extras
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -10,7 +11,30 @@ with open('config.json', 'r') as f:
 db = psycopg2.connect(host=config['database']['host'], database=config['database']['name'], user=config['database']['role'], password=config['database']['password'], cursor_factory=psycopg2.extras.RealDictCursor)
 
 
+def require_token(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        token = request.args.get('token', default=None)
+        if token == None or get_service(token) == None:
+            abort(403)
+        return fn(*args, **kwargs)
+    return wrapper
+
+def get_service(token):
+    with db.cursor() as cur:
+        cur.execute('''
+            select name
+            from service
+            where crypt(%s, hash) = hash
+        ''', [token])
+        try:
+            return cur.fetchone()['name']
+        except TypeError:
+            return None
+
+
 @app.route("/users")
+@require_token
 def get_users():
     with db.cursor() as cur:
         cur.execute('''
@@ -21,6 +45,7 @@ def get_users():
 
 @app.route("/user")
 @app.route("/user/<username>")
+@require_token
 def get_user(username=None):
     if username is None:
         raise NotImplementedError("This is where we would use request.args to search LDAP, matching the arbitrary filter you provide in order to find the username, then we would return get_user(username=that_user).")
@@ -34,6 +59,7 @@ def get_user(username=None):
             return jsonify(cur.fetchone())
 
 @app.route("/transaction", methods=["POST"])
+@require_token
 def add_transaction():
     username = request.form['username']
     amount = float(request.form['amount'])
